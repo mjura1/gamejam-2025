72320fde141b0938542de5a8916f8b6d288a2105
Matic Jurač <juracmatic@gmail.com>
2025-12-12 22:48:32 +0100

    Character can't be moved outside of map

--- team-berry-game/Scripts/map_behaviour.gd
+++ team-berry-game/Scripts/map_behaviour.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var tile_selector = get_node("/root/Node/TileSelector")
 @onready var grid_manager = get_node("/root/Node/GridManager")
+@onready var tile_map = get_node("/root/Node/Map/TileMapLayer")
 
 var selected_character: Node = null  # currently selected character
 
@@ -9,13 +10,20 @@ func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		var mouse_pos = get_global_mouse_position()
 		var clicked_grid = grid_manager.world_to_grid(mouse_pos)
+		var used_rect = tile_map.get_used_rect()
+		
+		var used_rect_x_min = used_rect.position.x
+		var used_rect_y_min = used_rect.position.y
+		var used_rect_x_max = used_rect.size.x
+		var used_rect_y_max = used_rect.size.y
 		
 		# Highlight the clicked tile
 		tile_selector.select_tile(clicked_grid)
 		
 		if selected_character:
 			# Move the selected character if the tile is empty
-			if not grid_manager.is_occupied(clicked_grid):
+			if not grid_manager.is_occupied(clicked_grid) and clicked_grid.x >= used_rect_x_min and clicked_grid.x <= used_rect_x_max and clicked_grid.y >= used_rect_y_min and clicked_grid.y <= used_rect_y_max:
+				
 				# Vacate old tile
 				grid_manager.vacate(selected_character.grid_pos)
 				# Update grid position
--- team-berry-game/Scripts/moveable_character.gd
+++ team-berry-game/Scripts/moveable_character.gd
@@ -5,4 +5,3 @@ var selected: bool = false
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		selected = true
-		print("Character selected at ", grid_pos)
