3d0c466f30bb199d4c9dbc4664ad649b58a53bca
Miha <miha.bravc@gmail.com>
2025-12-13 20:26:10 +0100

    Changed pawn movement

--- team-berry-game/Scripts/CharacterPieces/Ally/pawn.gd
+++ team-berry-game/Scripts/CharacterPieces/Ally/pawn.gd
@@ -6,9 +6,12 @@ func _ready():
 
 func get_move_directions() -> Array[Vector2i]:
 	return [
-		Vector2i(1, 0),
-		Vector2i(-1, 0),
-		Vector2i(0, 1),
-		Vector2i(0, -1),
+		Vector2i( 1,  0), 
+		Vector2i(-1,  0), 
+		Vector2i( 0,  1), 
+		Vector2i( 0, -1),
+		Vector2i( 1,  1), 
+		Vector2i( 1, -1),
+		Vector2i(-1,  1),
+		Vector2i(-1, -1),
 	]
-# calculate_valid_targets() se podeduje
