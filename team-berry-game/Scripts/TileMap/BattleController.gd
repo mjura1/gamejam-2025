fe908073eb0c74356b67319cc625decea856d1de
Miha <miha.bravc@gmail.com>
2025-12-14 21:45:32 +0100

    Added Snow of War
    grid manager script controls snow of war

--- team-berry-game/Scripts/TileMap/BattleController.gd
+++ team-berry-game/Scripts/TileMap/BattleController.gd
@@ -1,124 +1,104 @@
-# res://Scripts/BattleController.gd
 extends Node
+class_name BattleController
 
-# Stanja igre: Kdo je na vrsti.
-enum TurnState {
+# ===============================================
+# REFERENCE (POPRAVLJENO)
+# ===============================================
+
+# POPRAVEK: Spremenjena pot za dostop do bratskega vozlišča GridManager
+# Predpostavka: BattleController in GridManager sta na isti ravni (npr. oba otroka vozlišča 'Battle')
+@onready var grid_manager: GridManager = get_node("../GridManager") 
+@onready var player_manager = get_node("/root/PlayerManager")
+
+# ENUM za stanja bitke
+enum BattleState {
+	INITIALIZING,
 	PLAYER_TURN,
-	ENEMY_TURN
+	ENEMY_TURN,
+	TURN_END,
+	GAME_OVER
 }
 
-var current_state = TurnState.PLAYER_TURN
+var current_state: int = BattleState.INITIALIZING
+var turn_count: int = 0
 
-# ===============================================
-# REFERENCE
-# ===============================================
 
-@onready var player_manager = get_node("/root/PlayerManager") # Globalni Singleton
-@onready var grid_manager = get_node("../GridManager") # Brat (sibling) vozlišče
+# ----------------- INITIALIZATION -----------------
 
 func _ready():
-	# Opomba: Klic za generiranje mape ni več potreben.
-	print("Igra se je začela! Na vrsti je igralec.")
+	# Inicializiramo logiko bitke, vključno z meglo
+	# ZAVAROVALO: Če referenca še vedno ne deluje, to prepreči crash
+	if is_instance_valid(grid_manager):
+		initialize_battle()
+	else:
+		push_error("BattleController: GridManager ni najden. Inicializacija bitke ni mogoča.")
+
+
+func initialize_battle():
+	# KRITIČNO: Klic funkcije za inicializacijo megle (Fog of War)
+	if is_instance_valid(grid_manager):
+		# 1. Pokrijemo celotno mapo z meglo
+		grid_manager.initialize_all_fog()
+		
+		# 2. Razkrijemo območje okoli začetnih figur
+		update_fog_after_turn_start()
+
 	start_player_turn()
 
-# ----------------- IGRALČEVE POTEZE -----------------
+# ----------------- TURN LOGIC -----------------
 
 func start_player_turn():
-	current_state = TurnState.PLAYER_TURN
-	print(">> Na vrsti je Igralec!")
+	turn_count += 1
+	current_state = BattleState.PLAYER_TURN
+	print(">>> ZAČETEK POTEZE IGRALCA (Turn %d)" % turn_count)
 
 func end_player_turn():
-	# 1. Preverjanje Game Over
-	if player_manager.active_party.is_empty():
-		print("GAME OVER - Igralec poražen.")
-		return
+	print("<<< KONEC POTEZE IGRALCA >>>")
 	
-	# 3. Preklop na AI
+	# Po koncu poteze igralca posodobimo meglo
+	update_fog_after_turn_start()
+	
+	# Preklopimo na naslednjo fazo (npr. nasprotnikovo potezo)
 	start_enemy_turn()
 
-# ----------------- SOVRAŽNIKOVE POTEZE -----------------
-
 func start_enemy_turn():
-	current_state = TurnState.ENEMY_TURN
-	print(">> Na vrsti je Sovražnik!")
+	current_state = BattleState.ENEMY_TURN
+	print(">>> ZAČETEK POTEZE SOVRAŽNIKA <<<")
 	
-	# Čakamo 0.5 sekunde za vizualno pavzo
-	var timer = Timer.new()
-	timer.one_shot = true
-	add_child(timer)
-	timer.start(0.5)
-	await timer.timeout
-	timer.queue_free()
+	# TU BI SPROŽILI KODO ZA AI
+	# ...
 	
-	# Izvršimo logiko AI-ja
-	execute_enemy_move()
+	# Za testiranje takoj preklopimo nazaj
+	end_enemy_turn()
 
 func end_enemy_turn():
-	# Preverjanje Game Over po sovražnikovi potezi
-	if player_manager.active_party.is_empty():
-		print("GAME OVER - Igralec poražen.")
-		return
+	print("<<< KONEC POTEZE SOVRAŽNIKA >>>")
 	
-	# Preklop nazaj na igralca
+	# Posodobimo meglo, preden se začne igralčeva poteza (če je prišlo do premikov)
+	update_fog_after_turn_start()
+	
 	start_player_turn()
 
-# DECENTRALIZIRANA AI LOGIKA
-func execute_enemy_move():
-	
-	if not is_instance_valid(grid_manager):
-		push_error("AI ne more dostopati do GridManagerja.")
-		end_enemy_turn()
-		return
+# ----------------- FOG OF WAR LOGIC -----------------
 
-	var all_characters = grid_manager.get_all_characters()
-	var enemy_pieces: Array = []
-	
-	# 1. Zberemo vse aktivne sovražne figure
-	for char in all_characters:
-		if char is BaseCharacter and char.is_enemy:
-			enemy_pieces.append(char)
-	
-	if enemy_pieces.is_empty():
-		print("Zmaga! Vsi sovražniki uničeni. (Game Win)")
+# Ta funkcija posodobi meglo na podlagi trenutnih pozicij figur
+func update_fog_after_turn_start():
+	if not is_instance_valid(grid_manager) or not is_instance_valid(player_manager):
 		return
-		
-	# 2. Iščemo najboljšo potezo med vsemi sovražniki
-	var best_move: Dictionary = {}
-	var piece_to_act: BaseCharacter = null # Katera figura bo izvedla potezo
+	
+	var reveal_positions: Array[Vector2i] = []
+	
+	# 1. Zberemo pozicije vseh figur zaveznikov
+	for char in player_manager.active_party:
+		if is_instance_valid(char):
+			var char_pos = char.grid_pos
+			
+			# 2. Izračunamo vsa polja, ki jih je treba razkriti (3x3 območje)
+			for x in range(-1, 2):
+				for y in range(-1, 2):
+					var new_pos = char_pos + Vector2i(x, y)
+					if new_pos not in reveal_positions:
+						reveal_positions.append(new_pos)
 
-	for enemy in enemy_pieces:
-		var move_suggestion = enemy.calculate_best_move() # KLJUČNI DECENTRALIZIRANI KLIC
-		
-		if move_suggestion.is_empty():
-			continue
-			
-		# Prioriteta: Vedno izberemo prvo najdeno ZAJETJE
-		if move_suggestion.has("move_type") and move_suggestion.move_type == "CAPTURE":
-			best_move = move_suggestion
-			piece_to_act = enemy
-			break # Ustavimo iskanje, saj zajetje ne bo prekašano
-			
-		# Če ni zajetja, si shranimo vsaj en premik (prvi najdeni)
-		elif piece_to_act == null:
-			best_move = move_suggestion
-			piece_to_act = enemy
-			
-	# 3. IZVEDBA POTEZE
-	if piece_to_act:
-		var target_pos = best_move.target_pos
-		
-		if best_move.move_type == "CAPTURE":
-			var target_char = grid_manager.get_character_at(target_pos)
-			piece_to_act.capture(target_char)
-			print("AI: Zajetje z %s na %s" % [piece_to_act.name, target_pos])
-			
-		elif best_move.move_type == "MOVE":
-			piece_to_act.try_move(target_pos)
-			print("AI: Premik z %s na %s" % [piece_to_act.name, target_pos])
-			
-		end_enemy_turn() # Poteza je končana po uspešni akciji
-		return
-		
-	# Če ni bilo najdeno nič
-	print("AI: Ni možnih potez. Preskok poteze.")
-	end_enemy_turn()
+	# 3. Naročimo GridManagerju, da razkrije (odstrani meglo) na teh poljih
+	grid_manager.reveal_area(reveal_positions)
--- team-berry-game/Scripts/TileMap/grid_manager.gd
+++ team-berry-game/Scripts/TileMap/grid_manager.gd
@@ -2,9 +2,19 @@
 extends Node
 class_name GridManager
 
+# ===============================================
+# FOG OF WAR (NOVO)
+# ===============================================
+
+# Pot do scene, ki predstavlja eno polje megle
+# Prilagodite pot, če je vaša scena shranjena drugje!
+const FOG_TILE_SCENE: PackedScene = preload("res://Battle/fog_tile_scene.tscn")
+
+# Slovar za shranjevanje vozlišč megle. Key: Vector2i, Value: FogTile node
+var fog_nodes: Dictionary = {}
+
 # REFERENCE:
 @onready var player_manager = get_node("/root/PlayerManager")
-# OPOMBA: Če figure nimajo BaseCharacter skripte, boste morali GridManager dodati BaseCharacter referenco tukaj.
 
 # Size of one grid cell (match your TileMap)
 var cell_size: Vector2 = Vector2(16, 16)
@@ -12,10 +22,16 @@ var cell_size: Vector2 = Vector2(16, 16)
 # Stores objects by their grid location
 var occupied := {} # Primer: occupied[Vector2i(3,4)] = character reference
 
+# KRITIČNO: Deklaracija TileMap vozlišča za Godot 4. Uporabimo Node za lažje povezovanje.
+@export var tile_map: Node = null
+
 # ----------------- INITIALIZATION -----------------
 
 func _ready():
-	# Pokličemo funkcijo za registracijo figur, ko je scena naložena.
+	# NOVO DEBUG SPOROČILO ZA PREVERJANJE STANJA REFERENCE
+	print("DEBUG: GridManager ready. TileMap referenca (v ready): " + str(is_instance_valid(tile_map)))
+	
+	# Opomba: Prepričajte se, da je tile_map nastavljen v Inšpektorju ali kodi pred tem klicem.
 	register_all_characters_in_scene()
 	print("GridManager: Pripravljen.")
 
@@ -41,8 +57,9 @@ func register_all_characters_in_scene():
 	var found_allies = 0
 	
 	for node in character_nodes:
-		print("Najdeno vozlišče v skupini 'characters': " + node.name) 
+		print("Najdeno vozlišče v skupini 'characters': " + node.name)
 		
+		# Predpostavlja, da obstaja skripta BaseCharacter
 		if node is BaseCharacter:
 			var char = node as BaseCharacter
 			
@@ -50,9 +67,7 @@ func register_all_characters_in_scene():
 			char.grid_manager = self
 			
 			# 2. KRITIČNO NOVO: Inicializacija mreže se ZDAJ zgodi v figuri
-			char.on_grid_manager_registered() 
-			
-			# (Opomba: grid_pos in occupy() se zdaj izvajata znotraj char.on_grid_manager_registered())
+			char.on_grid_manager_registered()
 			
 			# 3. Registracija v PlayerManager
 			if not char.is_enemy:
@@ -60,6 +75,7 @@ func register_all_characters_in_scene():
 				found_allies += 1
 		else:
 			print("Opozorilo: Vozlišče v skupini 'characters' ni BaseCharacter: " + node.name)
+			
 # ----------------- GRID UTILITY FUNCTIONS -----------------
 
 func world_to_grid(world_pos: Vector2) -> Vector2i:
@@ -73,9 +89,6 @@ func is_occupied(grid_pos: Vector2i) -> bool:
 
 func occupy(grid_pos: Vector2i, obj):
 	if is_occupied(grid_pos):
-		# Pustimo opozorilo, vendar ne prekinemo, če je figura že tam.
-		# To se lahko zgodi, če BaseCharacter._ready in GridManager._ready kličeta hkrati.
-		# push_error("Trying to occupy already occupied tile " + str(grid_pos))
 		pass
 	else:
 		occupied[grid_pos] = obj
@@ -94,6 +107,78 @@ func is_inside_boundary(grid_pos: Vector2i, used_rect: Rect2i) -> bool:
 func get_character_at(grid_pos: Vector2i) -> Node:
 	return occupied.get(grid_pos, null)
 
-# Funkcija za AI/BattleController
 func get_all_characters() -> Array:
 	return occupied.values()
+
+# ===============================================
+# FOG OF WAR LOGIKA (NOVO)
+# ===============================================
+
+# Klicano s strani BattleControllerja, da na novo inicializira meglo
+func initialize_all_fog():
+	print("DEBUG FOG: Klic initialize_all_fog().")
+	
+	if not is_instance_valid(tile_map):
+		push_error("TileMap ni nastavljen v GridManagerju. Inicializacija megle ni mogoča.")
+		print("DEBUG FOG: Napaka! tile_map je neveljaven ali null.")
+		return
+		
+	print("DEBUG FOG: tile_map je veljaven. Začenjam generiranje.")
+	
+	# Odstranimo vso staro meglo, če obstaja
+	clear_all_fog()
+	
+	# Predpostavljamo, da tile_map.get_used_rect() deluje
+	var used_rect = tile_map.get_used_rect()
+	var fog_layer_id = 0 # Ta ID ni več uporabljen v klicu get_cell_source_id
+	
+	for x in range(used_rect.position.x, used_rect.end.x):
+		for y in range(used_rect.position.y, used_rect.end.y):
+			var tile_pos = Vector2i(x, y)
+			
+			# POPRAVLJENA VRSTICA 140: Odstranili smo fog_layer_id iz klica
+			if tile_map.get_cell_source_id(tile_pos) != -1:
+				_spawn_fog_tile(tile_pos)
+	
+	print("GridManager: Megla inicializirana na %d poljih." % fog_nodes.size())
+
+
+# Odstrani vsa vozlišča megle
+func clear_all_fog():
+	# Uporabimo keys() za varno iteracijo, medtem ko brišemo elemente
+	for pos in fog_nodes.keys():
+		_remove_fog_tile(pos)
+	fog_nodes.clear()
+	
+# Ustvari vozlišče megle na določeni mreži
+func _spawn_fog_tile(grid_pos: Vector2i):
+	if fog_nodes.has(grid_pos):
+		return # Megla že obstaja
+	
+	var fog_node = FOG_TILE_SCENE.instantiate()
+	
+	# Pozicioniranje
+	fog_node.position = grid_to_world(grid_pos)
+	
+	# POPRAVEK ZA NAPAKO "Parent node is busy": Uporaba call_deferred()
+	# To zagotavlja, da se vozlišče megle doda šele, ko starševsko vozlišče (Battle scena) konča s svojo inicializacijo.
+	get_parent().call_deferred("add_child", fog_node)
+	
+	fog_nodes[grid_pos] = fog_node
+
+# Odstrani vozlišče megle na določeni mreži
+func _remove_fog_tile(grid_pos: Vector2i):
+	if fog_nodes.has(grid_pos):
+		var fog_node = fog_nodes.get(grid_pos)
+		if is_instance_valid(fog_node):
+			# Znebimo se vozlišča, da ga ne riše več
+			fog_node.queue_free()
+		fog_nodes.erase(grid_pos)
+		return true
+	return false
+
+# Klicano s strani BattleControllerja za razkrivanje območja
+func reveal_area(positions_to_reveal: Array[Vector2i]):
+	for pos in positions_to_reveal:
+		# Odstrani vozlišče megle, če obstaja
+		_remove_fog_tile(pos)
